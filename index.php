c33ad9a4d4edd51c8d37224b56a6aaedb1d946b4
Braian Maciel <braian@Braians-MacBook-Pro.local>
2020-11-03 12:56:38 -0300

    Fixing heroku.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello World!";
+/* require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/libs/model.php';
+require_once __DIR__.'/models/userModel.php';
+require_once __DIR__.'/libs/controller.php';
+require_once __DIR__.'/libs/router.php';
+require_once __DIR__.'/libs/view.php';
+require_once __DIR__.'/libs/app.php';
+require_once __DIR__.'/libs/database/database.php';
+require_once __DIR__.'/controllers/routes.php';
+
+use app\libs\app\App;
+
+$app = new App($router);
+echo $app->run(); */
+
+?>
