95e26f6e3a6c2d0474ae29f11c61e6e06532456f
Braian Maciel <braian@Braians-MacBook-Pro.local>
2020-11-03 12:47:54 -0300

    Heroku test

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
+echo "Hello World!";
+/* require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/libs/model.php';
 require_once __DIR__.'/models/userModel.php';
 require_once __DIR__.'/libs/controller.php';
@@ -12,6 +13,6 @@ require_once __DIR__.'/controllers/routes.php';
 use app\libs\app\App;
 
 $app = new App($router);
-echo $app->run();
+echo $app->run(); */
 
 ?>
